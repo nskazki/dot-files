e2637628c39c53fc86e11f6aefc7726a5d08361e
nskazki <nskazki@gmail.com>
2017-12-16 22:34:13 +0300

    add image preview to fzf

--- .bash-tools/fzf.bash
+++ .bash-tools/fzf.bash
@@ -15,11 +15,12 @@ fi
 source "/home/nskazki/.fzf/shell/key-bindings.bash"
 
 export FZF_EXCLUDE='-E .git -E .npm -E node_modules/ -E .cache/ -E cache/ -E .tmp/ -E tmp/ -E bower_components/ -E coverage/ -E .gem/ -E .nvm/ -E .rbenv/ -E coverage_unit/ -E surefire-reports/ -E coverage-reports/'
+
 export FZF_DEFAULT_COMMAND='git branch > /dev/null 2>&1 && ([ "$(git root)" == "$HOME" ] && fd -H -I -t f '"$FZF_EXCLUDE"' || fd -H -t f '"$FZF_EXCLUDE"') || fd -I -H -t f '"$FZF_EXCLUDE"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
-export FZF_DEFAULT_OPTS='--height 70%'
 export FZF_CTRL_T_OPTS="--height 70% \
-                        --preview 'file --mime {} | grep binary > /dev/null 2>&1 \
-                                    && echo {} is a binary file \
-                                    || (highlight -O ansi -l {} || coderay {} || cat {}) 2> /dev/null | head -500'"
+                        --preview '((echo $(basename {}) | grep .svg > /dev/null 2>&1 && rsvg {} tmp/preview.png && img2txt tmp/preview.png) \
+                                     || (file --mime {} | grep image > /dev/null 2>&1 && img2txt {}) \
+                                     || (file --mime {} | grep binary > /dev/null 2>&1 && echo {} is a binary file) \
+                                     || (highlight -O ansi -l {} || coderay {} || cat {})) 2> /dev/null | head -500'"
