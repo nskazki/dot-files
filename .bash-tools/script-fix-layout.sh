98323cb347e2e134d761e3ba3eaf6d667cfda9f4
nskazki <nskazki@gmail.com>
2017-12-29 19:43:20 +0300

    add loop mode to script-fix-layout

--- .bash-tools/script-fix-layout.sh
+++ .bash-tools/script-fix-layout.sh
@@ -2,9 +2,22 @@
 
 export DISPLAY=:0
 
-if [ -f "/usr/bin/setxkbmap" ] && [ -n "$XDG_DATA_DIRS" ]; then
-  if [ -z "$(setxkbmap -v 6 | grep -s shift_caps_switch)" ]; then
-    /usr/bin/setxkbmap -layout "us,ru" -option "grp:shift_caps_switch,grp_led:caps"
-    numlockx "on"
+function fix-layout {
+  if [ -f "/usr/bin/setxkbmap" ] && [ -n "$XDG_DATA_DIRS" ]; then
+    if [ -z "$(setxkbmap -v 6 | grep -s shift_caps_switch)" ]; then
+      if [ -n "$(xset -q | grep -P "Caps Lock:\s+on")" ]; then
+        xdotool key Caps_Lock
+      fi
+      /usr/bin/setxkbmap -layout "us,ru" -option "grp:shift_caps_switch,grp_led:caps"
+    fi
   fi
+}
+
+if [ "$1" == "-l" ]; then
+  while true; do
+    fix-layout
+    sleep 0.5
+  done
+else
+  fix-layout
 fi
