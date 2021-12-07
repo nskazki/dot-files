6348031d90cb836cace39e112d14a7d707086c75
nskazki <nskazki@gmail.com>
2021-12-07 22:26:03 +0300

    Fix ls

--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,3 +1,7 @@
 function ls
-  command ls -h --color=auto --group-directories-first -1 $argv
+  if [ -x (command -v gls) ]
+    gls -h --color=auto --group-directories-first -1 $argv
+  else
+    command ls -h --color=auto --group-directories-first -1 $argv
+  end
 end
