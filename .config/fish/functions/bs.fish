a5f4b9e797e09900d1a596eeb6b2d288b081c83e
Nikita Skazki <nskazki@gmail.com>
2022-11-26 00:22:15 +0100

    Start Mailcatcher when starting Gleam

--- .config/fish/functions/bs.fish
+++ .config/fish/functions/bs.fish
@@ -1,3 +1,4 @@
 function bs
-  rm -rf public/assets && bundle install && bundle exec rake dashboard:stylesheets && TRUST_NETWORK=true bundle exec rails $argv s
+  mailcatcher
+  rm -rf public/assets && bundle install && bundle exec rake dashboard:stylesheets && TRUST_NETWORK=true bundle exec rails $argv s --config /Volumes/Repos/GleamSmtp/config.ru
 end
