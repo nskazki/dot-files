13c77f7f89793dfe84ea670add3e2cd1f65d6492
Nikita Skazki <nskazki@gmail.com>
2023-01-15 11:22:45 +0100

    Make the selection of a commit handier

--- .config/fish/functions/gh.fish
+++ .config/fish/functions/gh.fish
@@ -3,12 +3,12 @@ function gh
     return
   end
 
-  git log --graph --color=always --format='%C(magenta)%h%C(auto)%d%C(reset) %s' \
+  git log --first-parent --color=always --format='%C(magenta)%h%C(auto)%d%C(reset) %s' \
     | SHELL=bash fzf --preview \
-        'commit="$(echo {} | grep -Eo " [a-z0-9]+ " | head -n 1 | sed "s/ //g")"
+        'commit="$(echo {} | grep --color=never -o "^[a-z0-9]\{7,\}")"
 
          if [ -n "$commit" ]; then
-           git view-commit "$commit"
+           git view-commit "$commit" | ~/.scripts/diffr-with-custom-colors.sh
          fi' \
-    | grep --color=never -o "[a-f0-9]\{7,\}"
+    | grep --color=never -o "^[a-f0-9]\{7,\}"
 end
