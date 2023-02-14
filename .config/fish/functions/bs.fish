372ee360e55ad12fd9e0a8401e02af6c0d185c3a
Nikita Skazki <nskazki@gmail.com>
2023-02-14 23:53:24 +0100

    Hotfix mailcatcher

--- .config/fish/functions/bs.fish
+++ .config/fish/functions/bs.fish
@@ -1,4 +1,7 @@
 function bs
+  rbenv shell 2.7.7
   mailcatcher
+  rbenv shell --unset
+
   rm -rf public/assets && bundle install && bundle exec rake dashboard:stylesheets && TRUST_NETWORK=true bundle exec rails $argv s --config /Volumes/Repos/GleamSmtp/config.ru
 end
