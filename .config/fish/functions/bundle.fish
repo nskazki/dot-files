720cd75e0d65184cd1d471d3bd9c29a9f6846e61
Nikita Skazki <nskazki@gmail.com>
2023-03-02 23:21:45 +0100

    Rid of hacks I moved the codebase

--- .config/fish/functions/bundle.fish
+++
@@ -1,8 +0,0 @@
-function bundle
-  if string match -q exec -- $argv[1] && string match -q rake -- $argv[2]
-    rake $argv[3..-1]
-  else
-    command bundle $argv
-  end
-end
-
--- .config/fish/functions/rake.fish
+++ .config/fish/functions/rake.fish
@@ -1,9 +1,3 @@
 function rake
-  set Gleam (upward Gleam)
-
-  if present $Gleam
-    command bundle exec rake -E "require '$Gleam/config/environment.rb'; Rails.logger = Logger.new(STDOUT); Rails.logger.level = ENV.fetch('LOG_LEVEL', 'DEBUG')" $argv
-  else
-    command bundle exec rake $argv
-  end
+  bundle exec rake $argv
 end
