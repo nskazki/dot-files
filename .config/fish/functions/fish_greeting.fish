6ac8c49cc5d835c06e4e24d85ef0930462814c7c
Nikita Skazki <nskazki@gmail.com>
2023-03-17 00:36:12 +0100

    Bring back the cats!

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,12 +1,15 @@
 function fish_greeting
   # https://seethingswarm.itch.io/catset
-  # find ~/Documents/catset_gifs \( -name '*hurt*' -or -name '*dei*' -or -name '*ledge*' -or -name '*land*' -or -name '*fall*' -or -name '*dash*' -or -name '*wall*' -or -name '*jump*' -or -name '*crouch*' -or -name '*fright*' -or -name '*idle_8*'  \) -print -delete
-  # for file in (find ~/Documents/catset_gifs -name '*.gif'); convert $file -filter point -resize 1000% $file; end
-  # for file in (find ~/Documents/catset_gifs -name '*.gif'); convert $file $file.png; end
+  # https://github.com/maandree/util-say
+  #
+  # find ~/Documents/catset_gifs \( -name '*attack*' -or -name '*hurt*' -or -name '*die*' -or -name '*ledge*' -or -name '*land*' -or -name '*fall*' -or -name '*dash*' -or -name '*wall*' -or -name '*jump*' -or -name '*crouch*' -or -name '*fright*' -or -name '*idle_8*'  \) -print -delete
   # find ~/Documents/catset_gifs \( -name '*[12][0-9].png' -or -name '*8.png' -or -name '*9.png' \) -print -delete
+  # shopt -s globstar
+  # for file in ~/Documents/catset_gifs/**/*.png; do
+  #   ponytool --import image --magnified 1 --file $file --balloon n --export ponysay --balloon n --file - --chrome 1 --platform xterm > $file.pony
+  # done
 
-  # TODO: try again after the v3.5 release
-  if false && test -d ~/Documents/catset_gifs && test $PWD = $HOME
-    imgcat (random choice (find ~/Documents/catset_gifs -name '*.png'))
+  if test -d ~/Documents/catset_gifs
+    cat (random choice (find ~/Documents/catset_gifs -name '*.pony'))
   end
 end
