72df3e47c0baa65d6a4f62f3c895a97f639cae69
Nikita Skazki <nskazki@gmail.com>
2023-04-15 00:11:10 +0200

    Redo the ruby helpers

--- .config/fish/functions/bs.fish
+++ .config/fish/functions/bs.fish
@@ -1,7 +1,7 @@
 function bs
-  rbenv shell 2.7.7
-  mailcatcher
-  rbenv shell --unset
+  if ! pgrep -q -f (RBENV_VERSION=2.7.7 rbenv which mailcatcher)
+    RBENV_VERSION=2.7.7 rbenv exec mailcatcher
+  end
 
-  rm -rf public/assets && bundle install && bundle exec rake dashboard:stylesheets && TRUST_NETWORK=true bundle exec rails $argv s --config /Volumes/Repos/GleamSmtp/config.ru
+  rm -rf public/assets && bundle install && rake dashboard:stylesheets && TRUST_NETWORK=true rails $argv s --config /Volumes/Repos/GleamSmtp/config.ru
 end
