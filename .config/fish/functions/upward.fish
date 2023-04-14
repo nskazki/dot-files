f8f484caf589558cf82c03bb39d3ad4a7dea1480
Nikita Skazki <nskazki@gmail.com>
2023-04-15 00:11:17 +0200

    Fix the upward helper

--- .config/fish/functions/upward.fish
+++ .config/fish/functions/upward.fish
@@ -1,13 +1,18 @@
-function upward -a name
-  while true;
-    set path (realpath (string join / -- $depth $name))
+function upward
+  argparse 'q/quiet' -- $argv || return $status
+
+  while true
+    set path (path resolve (string join / -- $depth $argv))
 
     if string match -q $path $lasthpath
       return 1
     end
 
     if test -e $path
-      echo $path
+      if ! set -q _flag_quiet
+        echo $path
+      end
+
       return
     else
       set -a depth ..
