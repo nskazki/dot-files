de7128fb48082a86371aedea550f259885e496fb
Nikita Skazki <nskazki@gmail.com>
2023-05-03 01:20:11 +0200

    Fix the script clearing sublime snippets

--- .config/fish/functions/clear-sublime-snippets.fish
+++ .config/fish/functions/clear-sublime-snippets.fish
@@ -1,6 +1,5 @@
 function clear-sublime-snippets
   for snippet in (find "$HOME/Library/Application Support/Sublime Text 3/Packages/" -name "*.sublime-snippet")
-    echo "$snippet -> $snippet.backup"
-    mv $snippet $snippet.backup
+    echo > $snippet
   end
 end
