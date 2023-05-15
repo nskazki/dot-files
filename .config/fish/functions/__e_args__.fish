f952e05a905f756fa42a3f08409b857250051050
Nikita Skazki <nskazki@gmail.com>
2023-05-16 00:25:31 +0200

    Rework the e script

---
+++ .config/fish/functions/__e_args__.fish
@@ -0,0 +1,25 @@
+function __e_args__ -a lang
+  if string match -q -- $lang rb
+    set ruby
+  end
+
+  for arg in $argv[2..]
+    set clean_arg (strip-line-number $arg)
+    set clean_ext (path extension $clean_arg)
+
+    if ! path filter -f -q -- $clean_arg
+      color brblack "debug: couldn't find $arg" >&2
+      continue
+    end
+
+    if set -q ruby && ! string match -q -- '.rb' $clean_ext
+      continue
+    end
+
+    if ! set -q ruby && ! string match -q -r -- '.js|.vue' $clean_ext
+      continue
+    end
+
+    echo $clean_arg
+  end
+end
--- .config/fish/functions/e.fish
+++ .config/fish/functions/e.fish
@@ -1,12 +1,31 @@
 function e
-  if blank $argv
-    color brblack "yarn eslint" >&2
-    yarn eslint
-  else if any-ruby $argv
-    color brblack "rubocop $argv" >&2
-    rubocop $argv
+  if ! upward -q Gleam
+    return 1
+  end
+
+  if [ "$argv" = '.' ]
+    set paths (gm .)
   else
-    color brblack "eslint $argv" >&2
-    eslint $argv
+    set paths $argv
+  end
+
+  set js_paths (__e_args__ js $paths)
+  set rb_paths (__e_args__ rb $paths)
+
+  if blank $paths
+    echo (color brblack '$') 'eslint'
+    eslint
+    return $status
+  end
+
+  if present $js_paths
+    echo (color brblack '$') 'eslint' (color yellow $js_paths)
+    eslint $js_paths || return $status
+  end
+
+  if present $rb_paths
+    echo (color brblack '$') 'rubocop' (color yellow $rb_paths)
+    echo
+    rubocop $rb_paths || return $status
   end
 end
