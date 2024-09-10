0bb694e8040fbcc4a8ba9acb99333c5d620cac7c
Nikita Skazki <nskazki@gmail.com>
2024-09-10 23:32:45 +1000

    Install the latest version of mailcatcher

--- .config/fish/functions/bs.fish
+++ .config/fish/functions/bs.fish
@@ -1,6 +1,6 @@
 function bs
-  if ! pgrep -q -f (RBENV_VERSION=2.7.7 rbenv which mailcatcher)
-    RBENV_VERSION=2.7.7 rbenv exec mailcatcher
+  if ! pgrep -q -f (rbenv which mailcatcher)
+    rbenv exec mailcatcher
   end
 
   rm -rf public/assets && bundle install && rake dashboard:stylesheets && TRUST_NETWORK=true rails $argv s --config /Volumes/Repos/GleamSmtp/config.ru
