efcbdf4e2893e172b672b4858301ac7a844f7709
Nikita Skazki <nskazki@gmail.com>
2024-12-30 01:29:19 +1100

    Add AWS env vars to the right prompt

--- .config/fish/functions/fish_right_prompt.fish
+++ .config/fish/functions/fish_right_prompt.fish
@@ -1,23 +1,29 @@
 function fish_right_prompt
   set -l last_pipestatus $pipestatus
+  set -l last_duration $CMD_DURATION
 
-  if test "$last_generation" = $status_generation
-    return
+  if test "$last_generation" != $status_generation
+    if test (count $last_pipestatus) -gt 1
+      set -a prefix 'exit codes are '
+    else
+      set -a prefix 'exit code is '
+    end
+
+    set -a output (__fish_print_pipestatus $prefix '' '|' '' (set_color --bold $fish_color_status) $last_pipestatus)
+
+    if test "$last_duration" -ge 10000
+      set -a output 'done in' (color yellow (human-interval $last_duration))
+    end
   end
 
   set -g last_generation $status_generation
 
-  if test (count $last_pipestatus) -gt 1
-    set -a prefix 'exit codes are '
-  else
-    set -a prefix 'exit code is '
+  if present $AWS_REGION
+    set -a output (set_color black -b magenta)R:(string shorten -- $AWS_REGION)(set_color normal)
   end
 
-  set -a output (__fish_print_pipestatus $prefix '' '|' '' (set_color --bold $fish_color_status) $last_pipestatus)
-
-  set last_duration $CMD_DURATION
-  if test "$last_duration" -ge 10000
-    set -a output 'done in' (color yellow (human-interval $last_duration))
+  if present $AWS_PROFILE
+    set -a output (set_color black -b blue)P:(string shorten -- $AWS_PROFILE)(set_color normal)
   end
 
   if present $NODE_ENV
