498e1fd26cf559a75e9d7c5e02d7fe58f9da1a32
Nikita Skazki <nskazki@gmail.com>
2025-01-09 21:46:00 +1100

    Fix coffescript

--- .config/fish/functions/bs.fish
+++ .config/fish/functions/bs.fish
@@ -3,5 +3,5 @@ function bs
     tmux new-session -d -s my_mailtutan mailtutan
   end
 
-  rm -rf public/assets && bundle install && rake dashboard:stylesheets && rails $argv s --config /Volumes/Repos/GleamSmtp/config.ru
+  rm -rf public/assets && bundle install && rake dashboard:stylesheets && COFFEESCRIPT_SOURCE_PATH=scratch/scripts/coffee-script.js rails $argv s --config /Volumes/Repos/GleamSmtp/config.ru
 end
